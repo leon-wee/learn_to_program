f4efb51e8af0be72b1337961177f4dc76f50f0f3
Leon Wee <leonwee27@gmail.com>
2015-07-07 11:37:40 +0700

    Solved Ask Program

--- ch09-writing-your-own-methods/ask.rb
+++ ch09-writing-your-own-methods/ask.rb
@@ -1,3 +1,23 @@
+#!/Users/leonwee/.rvm/rubies/ruby-2.2.1/bin/ruby
 def ask question
-  # your code here
+  while true
+    puts question
+    answer = gets.chomp.downcase
+    if (answer == 'yes' || answer == 'no')
+      if answer == 'yes'
+        return true
+      else
+        return false
+      end
+      break
+    else
+      puts "Please answer 'yes' or 'no'."
+    end
+  end
 end
+
+ask 'Do you like eating tacos?'
+ask 'Do you like eating burritos?'
+puts ask 'Do you wet the bed?'
+ask 'Do you like eating chimichangas?'
+ask 'Do you like eating sopapillas.'
