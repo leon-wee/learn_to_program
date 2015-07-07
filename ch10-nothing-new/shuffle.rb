8edf972364a4c4d9957378b0456abbb1fb16937a
Leon Wee <leonwee27@gmail.com>
2015-07-08 01:03:54 +0700

    Added Shuffle

--- ch10-nothing-new/shuffle.rb
+++ ch10-nothing-new/shuffle.rb
@@ -1,3 +1,27 @@
+#!/Users/leonwee/.rvm/rubies/ruby-2.2.1/bin/ruby
 def shuffle arr
-  # your code here
+  shuffled = []
+
+  while arr.length > 0
+    rand_index = rand(arr.length)
+    curr_index = 0
+    new_arr = []
+
+    arr.each do |check|
+      if curr_index == rand_index
+        shuffled.push check
+      else
+        new_arr.push check
+      end
+      curr_index = curr_index + 1
+    end
+
+    arr = new_arr
+  end
+
+  shuffled
 end
+
+
+puts shuffle [1,2,3,4,5,6,7,8,9, 'For', 4.0]
+
